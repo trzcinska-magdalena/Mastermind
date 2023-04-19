49a7111667e4e7db8f47d258f8d3c8c0cf903e09
Magdalena Trzcińska <trzcinskamagdalena01@gmail.com>
2023-04-19 07:54:50 +0200

    Update Panel.js

--- js/Panel.js
+++ js/Panel.js
@@ -5,19 +5,26 @@ export class Panel {
     startY;
     gapX;
     gapY;
-
-    loop;
+    radius;
 
     blocks = new Array();
 
-    constructor(startX, gapX, startY, gapY) {
+    constructor(startX, gapX, startY, gapY, radius) {
         this.startX = startX;
         this.gapX = gapX;
         this.startY = startY;
         this.gapY = gapY;
+        this.radius = radius;
     }
 
-    addBlock(color) {
-        this.blocks.push(color);
+    addBlock(line, id, color) {
+        let midblockX = this.startX + this.gapX*id;
+        let midblockY = this.startY + this.gapY*line;
+
+        this.blocks.push(new Color(midblockX, midblockY, this.radius, color));
+    }
+
+    getBlock(id) {
+        return this.blocks[id];
     }
 }
